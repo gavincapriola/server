f65141596eaa44e461960d71d5ddf588acd4adc5
Gavin Capriola <82119122+gavincapriola@users.noreply.github.com>
2022-01-29 20:34:39 -0500

    Logging Incoming Requests to the Console

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,5 @@
-use std::net::TcpListener; // https://doc.rust-lang.org/std/net/struct.TcpListener.html
+use std::io::Read;
+use std::net::TcpListener; 
 
 pub struct Server { address: String }
 
@@ -7,9 +8,24 @@ impl Server {
     Server { address }
   }
 
-  pub fn start(&self) { 
+  pub fn start(self) { 
     println!("Starting server at {}", self.address); 
 
     let listener = TcpListener::bind(&self.address).unwrap();
+
+    loop {
+      match listener.accept() {
+        Ok((mut stream, _)) => {
+          let mut buffer = [0; 1024];
+          match stream.read(&mut buffer) {
+            Ok(_) => {
+              println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+            }
+            Err(e) => println!("Failed to read from connection: {}", e),
+          }
+        }
+        Err(e) => println!("Failed to establish a connection: {}", e),
+      }
+    }
   }
 }
