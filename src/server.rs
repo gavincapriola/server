86d64e94acb57baae1cf555c421ca71a4f10d808
Gavin Capriola <gavincapriola@icloud.com>
2022-01-29 21:07:09 -0500

    Traits and Type Conversions

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,5 @@
+use crate::http::Request;
+use std::convert::TryFrom;
 use std::io::Read;
 use std::net::TcpListener; 
 
@@ -20,6 +22,11 @@ impl Server {
           match stream.read(&mut buffer) {
             Ok(_) => {
               println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+              
+              match Request::try_from(&buffer[..]) {
+                Ok(request) => {},
+                Err(e) => println!("Failed to parse a request: {}", e),
+              }
             }
             Err(e) => println!("Failed to read from connection: {}", e),
           }
