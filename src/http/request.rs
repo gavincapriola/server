9d754979131e0d3014d4b8f13de4e00386f00bd4
Gavin Capriola <82119122+gavincapriola@users.noreply.github.com>
2022-01-31 19:18:56 -0500

    Custom Errors

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,5 +1,7 @@
 use super::method::Method;
 use std::convert::TryFrom;
+use std::error::Error;
+use std::fmt::{ Result as FmtResult, Display, Formatter, Debug };
 
 pub struct Request { 
   method: Method,
@@ -8,9 +10,41 @@ pub struct Request {
 }
 
 impl TryFrom<&[u8]> for Request {
-  type Error = String;
+  type Error = ParseError;
   
   fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
     unimplemented!();
   }
 }
+
+pub enum ParseError {
+  InvalidRequest,
+  InvalidEncoding,
+  InvalidProtocol,
+  InvalidMethod,
+}
+
+impl ParseError {
+  fn message(&self) -> &str {
+    match self {
+      Self::InvalidRequest => "InvalidRequest",
+      Self::InvalidEncoding => "InvalidEncoding",
+      Self::InvalidProtocol => "InvalidProtocol",
+      Self::InvalidMethod => "InvalidMethod",
+    }
+  }
+}
+
+impl Display for ParseError {
+  fn fmt(&self, f: &mut Formatter) -> FmtResult {
+    write!(f, "{}", self.message())
+  }
+}
+
+impl Debug for ParseError {
+  fn fmt(&self, f: &mut Formatter) -> FmtResult {
+    write!(f, "{}", self.message())
+  }
+}
+
+impl Error for ParseError {}
