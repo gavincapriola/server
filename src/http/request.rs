ccb7229c69439a01a20967eff4a9db57799e0f1c
Gavin Capriola <82119122+gavincapriola@users.noreply.github.com>
2022-02-02 20:42:10 -0500

    Converting an Option into a Result

--- src/http/request.rs
+++ src/http/request.rs
@@ -17,13 +17,21 @@ impl TryFrom<&[u8]> for Request {
   fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
     let request = str::from_utf8(buf)?;
 
+    let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+    let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+    let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+
+    if protocol != "HTTP/1.1" {
+      return Err(ParseError::InvalidProtocol)
+    }
+
     unimplemented!();
   }
 }
 
 fn get_next_word(request: &str) -> Option<(&str, &str)> {
   for (i, c) in request.chars().enumerate() { 
-    if c == ' ' {
+    if c == ' ' || c == '\r' {
       return Some((&request[..i], &request[i+1..]));
     }
   }
