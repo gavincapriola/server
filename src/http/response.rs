7edef13c01722c7e744b974c2f0fb9ff0f27ddcf
Gavin Capriola <gavincapriola@icloud.com>
2022-02-07 22:37:00 -0500

    HTTP Response

---
+++ src/http/response.rs
@@ -0,0 +1,15 @@
+pub enum StatusCode {}
+
+pub struct Response {
+  status_code: StatusCode,
+  body: Option<String>,
+}
+
+impl Response {
+  pub fn new(status_code: StatusCode, body: Option<String>) -> Self {
+    Response {
+      status_code: status_code,
+      body: body,
+    }
+  }
+}
