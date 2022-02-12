4daaecc1fd7e602e67c8cc97b6c833c13c754292
Gavin Capriola <82119122+gavincapriola@users.noreply.github.com>
2022-02-12 00:22:03 -0500

    Routing Incoming Requests

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,10 +1,16 @@
-use super::http::{ Request, Response, StatusCode };
+use super::http::{ Request, Response, StatusCode, Method };
 use super::server::Handler;
 
 pub struct WebSiteHandler;
 
 impl Handler for WebSiteHandler {
-  fn handle_request(&mut self, _request: &Request) -> Response {
-    Response::new(StatusCode::Ok, Some("<h1>Hello, world!</h1>".to_string()))
+  fn handle_request(&mut self, request: &Request) -> Response {
+    match request.method() {
+      Method::GET => match request.path() {
+        "/" => Response::new(StatusCode::Ok, Some("<h1>Hello, world!</h1>".to_string())),
+        _ => Response::new(StatusCode::NotFound, None),
+      },
+      _ => Response::new(StatusCode::NotFound, None),
+    }
   }
 }
